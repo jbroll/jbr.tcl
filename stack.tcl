85984273467826bedd243fb2d003c13adc36ebad
John Roll <john@rkroll.com>
2021-02-27 17:42:50 -0500

    lpush / lpop

--- stack.tcl
+++ stack.tcl
@@ -1,5 +1,5 @@
 
  # Stack Ops
  #
- proc push { S x } { upvar $S s; lappend s $x }                                               
- proc pop  { S   } { upvar $S s; lindex [list [lindex $s end] [set s [lrange $s 0 end-1]]] 0 } 
+ proc lpush { S x } { upvar $S s; lappend s $x }                                               
+ proc lpop  { S   } { upvar $S s; lindex [list [lindex $s end] [set s [lrange $s 0 end-1]]] 0 } 
