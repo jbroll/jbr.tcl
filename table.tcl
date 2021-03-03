0715913d8e15de15dcd7f1e5db96925199be8e47
John Roll <john@rkroll.com>
2021-03-02 20:36:42 -0500

    table sort

--- table.tcl
+++ table.tcl
@@ -1,7 +1,7 @@
 
 namespace eval table {
     namespace export header body row col nth todict tolist colnum setcell getcell justify rows cols
-    namespace export foreachrow
+    namespace export foreachrow sort colapp compute
 
     namespace ensemble create
 
@@ -28,13 +28,22 @@ namespace eval table {
     proc col { tbl args } {
         set header [header $tbl]
         set indicies [map i $args { lsearch $header $i }]
-        set hdr [lselect [tbl header] $indicies]
+        set hdr [lselect $header $indicies]
         set bdy [map row [body $tbl] { lselect $row $indicies }]
         list $hdr {*}$bdy
     }
     proc cols { tbl } { llength [lindex $tbl 0] }
     proc rows { tbl } { llength $tbl }
 
+    proc sort { tbl cols args } {
+        set header [header $tbl]
+        set indicies [map i $cols { lsearch $header $i }]
+        list $header {*}[lsort {*}$args -index $indicies [body $tbl]]
+    }
+
+    proc colapp { tbl col } {
+        list [list {*}[header $tbl] $col] {*}[map row [table body $tbl] { list {*}$row {} }]
+    }
     proc foreachrow { tbl rowName { script {} } } {
         if { $script eq "" } {
             set script $rowName
@@ -47,9 +56,18 @@ namespace eval table {
         }
     }
 
-    proc colnum { tbl colname } {
+    proc compute { tbl { script {} } } {
         set header [header $tbl]
-        lsearch $header $colname
+        set setvars "list [join [map col $header { I \$$col }] " "]"
+        list $header {*}[map row [body $tbl] {
+            uplevel [list lassign $row {*}$header]
+            uplevel $script
+            uplevel $setvars
+        }]
+    }
+
+    proc colnum { tbl colname } {
+        lsearch [header $tbl] $header $colname
     }
     proc nth { tbl row } {
         return [list [lindex $tbl 0] [lindex $tbl $row+1]]
