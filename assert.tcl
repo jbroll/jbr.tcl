54ccde4d712e927852761b920b65bd854a8350f0
John Roll <john@rkroll.com>
2021-04-25 11:06:52 -0400

    pass script and uplevel it in assert

--- assert.tcl
+++ assert.tcl
@@ -6,19 +6,19 @@ proc assert-fail { reason } {
 }
 
 proc assert-eq { va vb { msg "" } } {
-    if { $va != $vb } {
+    if { [uplevel 1 $va] != $vb } {
         assert-fail "failed assert $va != $vb : $msg"
     }
 }
 
 proc assert-true { va { msg "" } } {
-    if { !$va } {
+    if { ![uplevel 1 $va] } {
         assert-fail "failed assert $va not true : $msg"
     }
 }
 
 proc assert-false { va { msg "" } } {
-    if { $va } {
+    if { [uplevel 1 $va] } {
         assert-fail "failed assert $va not false : $msg"
     }
 }
