088d950c6372eb053ddd4dd4ff10f896d5173353
John Roll <john@rkroll.com>
2021-04-28 19:58:16 -0400

    start test counting report

--- test.tcl
+++ test.tcl
@@ -8,18 +8,10 @@ interp alias {} true {} assert-true
 interp alias {} false {} assert-false
 
 namespace eval test {
-    set SKIP 10
-    set FAIL 11
-    set ::ASSERT_FAIL $FAIL
-
-    proc skip { reason } {
-        return -code $test::SKIP $reason
-    }
-
-    proc platform { name eq value } {
-        if "\$::tcl_platform(\$name) $eq \$value" {
-        } else { skip "platform constraint $name $eq $value but is $::tcl_platform($name)" }
-    }
+    set setup {}
+    set SKIP_CODE 10
+    set FAIL_CODE 11
+    set ::ASSERT_FAIL $FAIL_CODE
 
     proc check-options { options args } {
         if { [llength $args] %2 } {
@@ -44,35 +36,66 @@ namespace eval test {
     }
 
     proc suite { name code } {
+        variable PASS 0
+        variable SKIP 0
+        variable WARN 0
+        variable FAIL 0
+
         print $name
         try {
             namespace eval ::test $code
-        } on $test::SKIP e {
+            incr PASS
+        } on $test::SKIP_CODE e {
             print " " SKIP suite $name: $e
+            incr SKIP
         }
+
+        print "  PASS" $PASS SKIP $SKIP WARN $WARN FAIL $FAIL
     }
 
+    proc setup { script } {
+        variable setup $script
+    }
+
+    proc skip { reason } {
+        return -code $test::SKIP_CODE $reason
+    }
+
+    proc platform { name eq value } {
+        if "\$::tcl_platform(\$name) $eq \$value" {
+        } else { skip "platform constraint $name $eq $value but is $::tcl_platform($name)" }
+    }
+
+
     proc test { name body } {
+        variable SKIP 
+        variable FAIL
+
         set options [check-options { skip setup cleanup } {*}$body]
+        variable setup
 
         set it [interp create]
         $it eval {
             package require jbr::test
             namespace path ::test
         }
+        $it eval $setup
 
         foreach {phase code} $options {
             if { $phase eq "skip" && $code ne "" } {
                 print "  SKIP test $name: $code"
+                incr SKIP
                 return
             }
             try {
                 $it eval $code
-            } on $test::FAIL {e options} {
+            } on $test::FAIL_CODE {e options} {
                 print " " FAIL $name in $phase $e $options
+                incr FAIL
                 return
-            } on $test::SKIP e {
+            } on $test::SKIP_CODE e {
                 print " " SKIP case $name in $phase $e
+                incr SKIP
             }
         }
         interp delete $it
