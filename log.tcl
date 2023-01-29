a90e0468193ff4e1cdc8b0c187ad9de3da18ad2b
John Roll <john@rkroll.com>
2023-01-29 14:42:18 +0000

    Oops print

--- log.tcl
+++ log.tcl
@@ -1,10 +1,8 @@
 
-package require jbr::print
-
 set APPNAME [file rootname [file tail $::argv0]]
 set LOGFILE [clock format [clock seconds] -format "%Y%m%d"]-$APPNAME.log
 
 proc log { msg } {
-    print echo $msg >> $::LOGFILE
+    echo $msg >> $::LOGFILE
 }
 
