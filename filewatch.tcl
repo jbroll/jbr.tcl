93227b437762346d72f66dbd5fcfe7d0b8611590
John Roll <john@rkroll.com>
2023-07-08 03:22:29 +0000

    Add filewatch.tcl

---
+++ filewatch.tcl
@@ -0,0 +1,19 @@
+
+proc filewatch { file command { interval 5000 } {mtime -} } {
+    if {$mtime eq "-"} {
+        if [info exists ::_twf:$file] {after cancel [set ::_twf:$file]}
+        filewatch $file $command $interval [file mtime $file]
+    } else {
+        set newtime [file mtime $file]
+        if {$newtime != $mtime} {
+            try $command on error e { 
+                print filewatch $file $e 
+                error $e
+            }
+            filewatch $file $command $interval [file mtime $file]
+        } else {
+            set ::_twf:$file [after $interval [info level 0]]
+        }
+    }
+}
+
