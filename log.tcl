308ffffccc3393a890ae1732fbb5376697d7823e
John Roll <john@rkroll.com>
2024-06-18 18:51:53 -0400

    use TclOO for logs

--- log.tcl
+++ log.tcl
@@ -1,66 +1,73 @@
 
 package require TclOO
 
-set HOME $env(HOME)
-::tcl::tm::path add $HOME/lib/tcl8/site-tcl
-
 package require jbr::unix
 package require jbr::tcloo
 package require jbr::print
 
-set APPNAME [file rootname [file tail $::argv0]]
-
 oo::class create logger {
 
-    constructor { { l info } } {
-	variable levels { fatal error warn info debug } 
+    constructor { { _level info } { files - } { _logpath . } } {
+        variable levels { fatal error warn info debug } 
 
-	variable level 
-	variable files
-	variable lineFormat
-	variable fileFormat
-	
-	set level [lsearch $levels $l]
-	set files [list LOGFILE - ]
-	set lineFormat {$time $file:$line $msg} 
-	set fileFormat {$date-$name.log}
+        variable level 
+        variable files
+        variable logpath
+        variable lineFormat
+        variable fileFormat
+        
+        set level [lsearch $levels $_level]
+        set files $_files
+        set logpath $_logpath
+        set lineFormat {$date $time $file:$line $msg} 
+        set fileFormat {$path/$date-$file.log}
     }
     
     private method levelValue { l } {
-	classvariable levels
-	return [lsearch $levels $l]
+        variable levels
+        return [lsearch $levels $l]
     }
 
     private method log { msgLevel msg } {
-	variable level
-	variable files
-	variable lineFormat
-	variable fileFormat
+        variable level
 
-	if { $level >= $msgLevel } {
-	    set frame [info frame -2]
-	    set path [dict get $frame file]
-	    set file [file tail $path]
-	    set line [dict get $frame line]
-	    set date [clock format [clock seconds] -format "%Y%m%d"]
-	    set time [clock format [clock seconds]]
-	    set name $::APPNAME
+        if { $level >= $msgLevel} {
+            variable files
+            variable lineFormat
+            variable fileFormat
 
-	    set msg [subst $lineFormat]
-	    set log [subst $fileFormat]
-	    foreach file $files {
-		print echo $msg >> $log
-	    }
-	}
+            set frame [info frame -2]
+            set path [dict get $frame file]
+            set file [file tail $path]
+            set line [dict get $frame line]
+            set date [clock format [clock seconds] -format "%Y%m%d"]
+            set time [clock format [clock seconds]]
+
+            set msg [subst $lineFormat]
+
+            set path $logpath
+            foreach file $files {
+                set log [subst $fileFormat]
+                print echo $msg >> $log
+            }
+        }
     }
 
     public method info  { msg } { my log 3 $msg }
     public method warn  { msg } { my log 2 $msg }
     public method error { msg } { my log 1 $msg }
+    public method error-info { msg } { my log 1 $msg; my log 1 $::errorInfo }
     public method level { l } { 
-	set level [my levelValue $l]
+        set level [my levelValue $l]
+    }
+    public files { _files } {
+        variable files
+        set files $_files
+    }
+    public path { _path } {
+        variable path
+        set path $_path
     }
 }
 	
-
 logger create log
